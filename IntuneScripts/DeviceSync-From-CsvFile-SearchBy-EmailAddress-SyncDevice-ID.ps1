531ce0c18952dec970737f40ce59c1b1948afdbd
E-stefon-M <127267255+E-stefon-M@users.noreply.github.com>
2024-03-21 17:20:23 -0700

    Update DeviceSync-From-CsvFile-SearchBy-EmailAddress-SyncDevice-ID.ps1

--- IntuneScripts/DeviceSync-From-CsvFile-SearchBy-EmailAddress-SyncDevice-ID.ps1
+++ IntuneScripts/DeviceSync-From-CsvFile-SearchBy-EmailAddress-SyncDevice-ID.ps1
@@ -1,7 +1,14 @@
-#SEARCHING FOR THE EMAIL THEN Syncing Each device associated with the emailaddress of user. 
-
+.SYNOPSIS
+  Sync specific devices
+.DESCRIPTION
+SEARCHING FOR THE EMAIL THEN Syncing Each device associated with the emailaddress of user.
 #Created By: Estefon Marrero
 #Date Created: 3/7/2023
+.NOTES
+  Version: V1
+  Author: Estefon Marrero
+  Revised: 3/21/2024
+#####################################################################################
 
 $emailAddresses = Import-Csv "PATH OF CSV FILE"
 
@@ -14,7 +21,8 @@ ForEach ($user in $emailAddresses) {
 
         Write-Host "Found Devices for $($user.emailAddress)." `n 
 
-            foreach ($device in $DeviceInfo) { #This will then loop through each of the devices the user has under them and send the sync command.
+#This will then loop through each of the devices the user has under them and send the sync command.
+            foreach ($device in $DeviceInfo) { 
                 $ManagedDeviceId = $Device.Id
                 Sync-MgDeviceManagementManagedDevice -ManagedDeviceId $ManagedDeviceId
                 Write-Host "ID $($ManagedDeviceId) with Name $($Device.DeviceName) is syncing."
